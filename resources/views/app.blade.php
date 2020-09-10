4b2aa0e88cb6af6c4eab4efdc80cce5b875e971a
polaris218 <software.dev0218@gmail.com>
2020-09-10 21:25:20 +0800

    update blade, htaccess

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,13 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Tasksman</title>
+        <title>Numbly</title>
         <!-- Styles -->
         <link href="{{ asset('css/index.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app"></div>
-
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
     </html>
